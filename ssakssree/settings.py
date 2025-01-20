165c05ea9cdf19b20835d72b6922ba092af16e49
dante <datapod.k@gmail.com>
2025-01-20 16:57:02 +0900

    secret 키 분리

--- ssakssree/settings.py
+++ ssakssree/settings.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^6bh89ly5-b&e^*kv0@v7zp@e-7rcuv=n8e&jgynr6!0h$ipv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
